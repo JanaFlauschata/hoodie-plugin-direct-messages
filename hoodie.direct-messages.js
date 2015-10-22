cb469830582586de4699c7950f15285436236b17
Jana Flauschata <jana.kutscheid@gmail.com>
2015-10-22 13:40:57 +0200

    use jQuery to get deferred object

--- hoodie.direct-messages.js
+++ hoodie.direct-messages.js
@@ -1,34 +1,34 @@
 Hoodie.extend(function(hoodie) {
-  
-  function send( messageData ) {
-    var defer = hoodie.defer();
 
-    hoodie.task.start('directmessage', messageData)
-    .done( function(messageTask) {
-      hoodie.task.on('remove:directmessage:'+messageTask.id, defer.resolve);
-      hoodie.task.on('error:directmessage:'+messageTask.id, defer.reject);
-    })
-    .fail( defer.reject );
+	function send( messageData ) {
+		var defer = $.Deferred();
 
-    return defer.promise();
-  }
-  
-  function findAll(){
-    var defer = hoodie.defer();
-    hoodie.store.findAll('message')
-    .done(defer.resolve)
-    .fail(defer.reject)
+		hoodie.task.start('directmessage', messageData)
+			.done( function(messageTask) {
+				hoodie.task.on('remove:directmessage:'+messageTask.id, defer.resolve);
+				hoodie.task.on('error:directmessage:'+messageTask.id, defer.reject);
+			})
+			.fail( defer.reject );
 
-    return defer.promise();
-  }
-  
-  function on( eventName, callback ) {
-    hoodie.task.on( eventName + ':directmessage', callback);
-  }
+		return defer.promise();
+	}
 
-  hoodie.directMessages = {
-    send: send,
-    findAll: findAll,
-    on: on
-  }
+	function findAll(){
+		var defer = hoodie.defer();
+		hoodie.store.findAll('message')
+			.done(defer.resolve)
+			.fail(defer.reject)
+
+		return defer.promise();
+	}
+
+	function on( eventName, callback ) {
+		hoodie.task.on( eventName + ':directmessage', callback);
+	}
+
+	hoodie.directMessages = {
+		send: send,
+		findAll: findAll,
+		on: on
+	}
 });
